f63b0af08aa91a6513eaa948886a91e19a810712
unknown <1234.alok@gmail.com>
2025-04-05 22:26:56 +0530

    hero import issue resolv

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Hero from "./component/Hero/hero.jsx";
+import Hero from "./component/Hero/Hero.jsx";
 import About from "./component/About/About.jsx";
 
 createRoot(document.getElementById("root")).render(
