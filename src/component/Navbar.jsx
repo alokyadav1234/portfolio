54709bfed3336e3e188ab3e768c608b64bbd14e9
unknown <1234.alok@gmail.com>
2025-04-07 00:06:10 +0530

    arrow added after line navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.svg";
+import round_arrow from "../assets/round_arrow.svg";
 import "./Navbar.css";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
@@ -19,40 +20,46 @@ const Navbar = () => {
       <div className="nav-logo">
         <img src={logo} alt="navlogo" />
       </div>
-      <div className="nav-line"></div>
-      {/* <span className="nav-arrow"></span> {`>`} */}
-      {/* Add horizontal line */}
-      <div
-        className="nav-menu-container"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <div className="nav-dot"></div> {/* Dot for toggle */}
-        <ul className={`nav-menu ${menuOpen ? "show" : "hide"}`}>
-          {/* <li>
+      <div className="dotline-container">
+        <div className="nav-line"></div>
+        {/* <span className="nav-arrow"></span> {`>`} */}
+        {/* Add horizontal line */}
+        <div
+          className="nav-menu-container"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {/* <div className="nav-dot"></div>  */}
+          {/* Dot for toggle */}
+          <div className="nav-dot">
+            <img src={round_arrow} />
+          </div>
+          <ul className={`nav-menu ${menuOpen ? "show" : "hide"}`}>
+            {/* <li>
             <AnchorLink className="anchor-link">Home</AnchorLink>
           </li> */}
-          <li>
-            <AnchorLink className="anchor-link" href="#about">
-              About Me
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink className="anchor-link" href="#services">
-              Projects
-            </AnchorLink>
-          </li>
-          {/* <li>
+            <li>
+              <AnchorLink className="anchor-link" href="#about">
+                About Me
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink className="anchor-link" href="#services">
+                Projects
+              </AnchorLink>
+            </li>
+            {/* <li>
             <AnchorLink className="anchor-link" href="mywork">
               Portfolio
             </AnchorLink>
           </li> */}
-          <li>
-            <AnchorLink className="anchor-link" href="#contact">
-              Contact
-            </AnchorLink>
-          </li>
-        </ul>
+            <li>
+              <AnchorLink className="anchor-link" href="#contact">
+                Contact
+              </AnchorLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
